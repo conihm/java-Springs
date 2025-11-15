05580c0b511a8d3839c94cbf6425b92e82ba86f6
Constanza Hurtado Meneses <constanza.hurtado.meneses@gmail.com>
2025-11-14 21:25:18 -0300

    first commit

---
+++ mi_primer_proyecto_spring_boot/src/main/java/com/constanza/mi_primer_proyecto_spring_boot/MiPrimerProyectoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.constanza.mi_primer_proyecto_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiPrimerProyectoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiPrimerProyectoSpringBootApplication.class, args);
+	}
+
+}
---
+++ mi_primer_proyecto_spring_boot/src/test/java/com/constanza/mi_primer_proyecto_spring_boot/MiPrimerProyectoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.constanza.mi_primer_proyecto_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiPrimerProyectoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
